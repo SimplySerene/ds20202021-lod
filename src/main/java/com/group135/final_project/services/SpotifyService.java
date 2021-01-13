05a339124fa3aac4499d69af479187d9f8982b68
Niclas van Eyk <niclas.eyk@gmail.com>
2021-01-13 18:16:32 +0100

    Artists should be unique now

--- src/main/java/com/group135/final_project/services/SpotifyService.java
+++ src/main/java/com/group135/final_project/services/SpotifyService.java
@@ -9,7 +9,9 @@ import org.apache.hc.core5.http.ParseException;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,14 +37,25 @@ public class SpotifyService {
             .getTracks()
             .getItems();
 
-        // TODO: As we convert this to a list and not a set, an artist might
-        //       appear twice. We need to figure out how to filter out duplicates
-        return Arrays.stream(items)
+        var artists = Arrays.stream(items)
                 .map(PlaylistTrack::getTrack)
                 .filter(track -> track instanceof Track)
                 .map(item -> (Track) item)
                 .flatMap(track -> Arrays.stream(track.getArtists()))
                 .collect(Collectors.toList());
+
+        // the ArtistSimplified class unfortunately does not implement hashcode or equals...
+        var ids = new HashSet<String>();
+        var uniqueArtists = new ArrayList<ArtistSimplified>();
+
+        for (var artist : artists) {
+            if (!ids.contains(artist.getId())) {
+                ids.add(artist.getId());
+                uniqueArtists.add(artist);
+            }
+        }
+
+        return uniqueArtists;
     }
 
     public PlaylistSimplified[] searchPlaylists(String query) throws ParseException, SpotifyWebApiException, IOException {
