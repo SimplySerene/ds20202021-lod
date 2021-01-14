20cc66fe9a1651a40f7c9d7da934450f49f899ad
Timme Bethe <timme.bethe@gmail.com>
2021-01-14 13:49:59 +0100

    formatting

--- src/main/java/com/group135/final_project/Test.java
+++ src/main/java/com/group135/final_project/Test.java
@@ -12,7 +12,12 @@ public class Test {
     
 
     private static String str = """ 
-    "1McMsnEElThX1knmY4oliG", "4q3ewBCX7sLwd24euuV69X", "0EFisYRi20PTADoJrifHrz", "1Xyo4u8uXC1ZmMpatF05PJ", "6fWVd57NKTalqvmjRd2t8Z", "6ASri4ePR7RlsvIQgWPJpS", "790FomKkXshlbRYZFtlgla", "66CXWjxzNUsdJxJ2JdwvnR", "66CXWjxzNUsdJxJ2JdwvnR", "1uNFoZAHBGtllmzznpCI3s", "6MPCFvOQv5cIGfw3jODMF0", "2hlmm7s2ICUX0LVIhVFlZQ", "4Gso3d4CscCijv0lmajZWs", "7rkW85dBwwrJtlHRDkJDAC", "4q3ewBCX7sLwd24euuV69X", "7ltDVBr6mKbRvohxheJ9h1", "7arQA31aZVS8yS6zUveWzb", "2tIP7SsRs7vjIcLrU85W8J", "3Nrfpe0tUJi4K4DXYWgMUX", "7tYKF4w9nC0nq9CsPZTHyP", "6qqNVTkY8uBg9cP3Jd7DAH", "6M2wZ9GZgrQXHCFfjv46we", "4r63FhuTkUYltbVAg5TQnk", "7iK8PXO48WeuP03g8YR51W" 
+    "1McMsnEElThX1knmY4oliG", "4q3ewBCX7sLwd24euuV69X", "0EFisYRi20PTADoJrifHrz", "1Xyo4u8uXC1ZmMpatF05PJ", 
+    "6fWVd57NKTalqvmjRd2t8Z", "6ASri4ePR7RlsvIQgWPJpS", "790FomKkXshlbRYZFtlgla", "66CXWjxzNUsdJxJ2JdwvnR", 
+    "66CXWjxzNUsdJxJ2JdwvnR", "1uNFoZAHBGtllmzznpCI3s", "6MPCFvOQv5cIGfw3jODMF0", "2hlmm7s2ICUX0LVIhVFlZQ", 
+    "4Gso3d4CscCijv0lmajZWs", "7rkW85dBwwrJtlHRDkJDAC", "4q3ewBCX7sLwd24euuV69X", "7ltDVBr6mKbRvohxheJ9h1", 
+    "7arQA31aZVS8yS6zUveWzb", "2tIP7SsRs7vjIcLrU85W8J", "3Nrfpe0tUJi4K4DXYWgMUX", "7tYKF4w9nC0nq9CsPZTHyP", 
+    "6qqNVTkY8uBg9cP3Jd7DAH", "6M2wZ9GZgrQXHCFfjv46we", "4r63FhuTkUYltbVAg5TQnk", "7iK8PXO48WeuP03g8YR51W" 
     """;
     private static List<String> spotifyIds = Arrays.asList(str.split("\\s*,\\s*"));
 
